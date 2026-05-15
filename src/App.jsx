e854cde85de3c413f0b7027a82a3a1bfb7c8e7ce
Abhishek Singh <abhi16.2007@gmail.com>
2026-05-15 18:19:51 +0530

    Modify C++ code snippet in App.jsx
    Updated C++ code snippet for longest increasing subsequence algorithm in App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ int main() {
     printf("Element found at index: %d\n", result);
     return 0;
 }`,
-  cpp: "#include <iostream>\n#include <vector>\n#include <algorithm>\nusing namespace std;\n\nint lis(vector<int>& nums) {\n    vector<int> tails;\n    for (int num : nums) {\n        auto it = lower_bound(tails.begin(), tails.end(), num);\n        if (it == tails.end()) tails.push_back(num);\n        else *it = num;\n    }\n    return tails.size();\n}\n\nint main() {\n    vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};\n    cout << \"LIS length: \" << lis(nums) << endl;\n    return 0;\n}",,
+  cpp: "#include <iostream>\n#include <vector>\n#include <algorithm>\nusing namespace std;\n\nint lis(vector<int>& nums) {\n    vector<int> tails;\n    for (int num : nums) {\n        auto it = lower_bound(tails.begin(), tails.end(), num);\n        if (it == tails.end()) tails.push_back(num);\n        else *it = num;\n    }\n    return tails.size();\n}\n\nint main() {\n    vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};\n    cout << \"LIS length: \" << lis(nums) << endl;\n    return 0;\n}",
   java: `import java.util.*;
 
 public class Main {
