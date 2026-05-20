c3a27c3fcef16b7c5ff2e53adf2351de3b2abd8d
Abhishek Singh <abhi16.2007@gmail.com>
2026-05-20 22:40:09 +0530

    Switch to hydrateRoot for server-rendered content

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,26 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot, hydrateRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+const container = document.getElementById('root')
+
+if (container.hasChildNodes()) {
+  hydrateRoot(container,
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  )
+} else {
+  const root = createRoot(container)
+  root.render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  )
+}
